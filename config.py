bb1bc0baed6fb926c62dba4280a25b8aeffb71d4
Sareeflonda88 <142931776+Sareeflonda88@users.noreply.github.com>
2025-06-22 17:07:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -57,7 +57,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Sareeflonda88/Nancy",
+    "https://github.com/Sareeflonda88",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
